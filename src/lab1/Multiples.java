d8e0ed946b877105a553970b7d6fd915ec805cba
Brooklyn Zhou <brooklyn.zhou@mail.utoronto.ca>
2025-09-09 00:05:49 -0400

    Multiple class added

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000,3,5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int count = 0;
+        while (i < n) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
+
+            if (divisibleBya|| divisibleByb) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
