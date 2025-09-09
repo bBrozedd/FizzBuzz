486cd4921b46827fcb083826ef3dbf1e6bec0205
Brooklyn Zhou <brooklyn.zhou@mail.utoronto.ca>
2025-09-09 00:44:54 -0400

    Create the Reduce.reduce method.

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -3,8 +3,13 @@ package lab1;
 public class Reduce {
     public static void main(String[] args) {
 
-        int count = 0;
         int n = 100;
+        int count = reduce(n);
+        System.out.println(count);
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
         while (n > 0) {
             if (n % 2 == 0) {
                 n = n / 2;
@@ -13,6 +18,6 @@ public class Reduce {
             }
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 }
